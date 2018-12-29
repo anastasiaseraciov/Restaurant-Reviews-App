1465b13415b3609f576c88f2fbf66ebb50599b88
Anastasia Seraciov <anastasia.seraciov@yahoo.com>
2018-12-29 00:38:27 +0000

    Add mapbox to show the map

--- js/main.js
+++ js/main.js
@@ -71,6 +71,7 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 /**
  * Initialize leaflet map, called from HTML.
  */
+
 initMap = () => {
   self.newMap = L.map('map', {
         center: [40.722216, -73.987501],
@@ -78,7 +79,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYW5hc3Rhc2lhODciLCJhIjoiY2pxOHE4c2F0MnVlMzQycGw0ODR1ejU2MiJ9.VUbKqBSo9Ih2ozGY1Q5kZQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
