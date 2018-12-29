bdfb82a71052bb52e33292de2a274371c98f4150
Anastasia Seraciov <anastasia.seraciov@yahoo.com>
2018-12-29 23:37:16 +0000

    Add ARIA labels

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -4,7 +4,7 @@ var newMap;
 /**
  * Initialize map as soon as the page is loaded.
  */
-document.addEventListener('DOMContentLoaded', (event) => {  
+document.addEventListener('DOMContentLoaded', (event) => {
   initMap();
 });
 
@@ -15,7 +15,7 @@ initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
       console.error(error);
-    } else {      
+    } else {
       self.newMap = L.map('map', {
         center: [restaurant.latlng.lat, restaurant.latlng.lng],
         zoom: 16,
@@ -27,14 +27,14 @@ initMap = () => {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox.streets'    
+        id: 'mapbox.streets'
       }).addTo(newMap);
       fillBreadcrumb();
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
     }
   });
-}  
- 
+};
+
 /* window.initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
@@ -56,12 +56,12 @@ initMap = () => {
  */
 fetchRestaurantFromURL = (callback) => {
   if (self.restaurant) { // restaurant already fetched!
-    callback(null, self.restaurant)
+    callback(null, self.restaurant);
     return;
   }
   const id = getParameterByName('id');
   if (!id) { // no id found in URL
-    error = 'No restaurant id in URL'
+    error = 'No restaurant id in URL';
     callback(error, null);
   } else {
     DBHelper.fetchRestaurantById(id, (error, restaurant) => {
@@ -71,10 +71,10 @@ fetchRestaurantFromURL = (callback) => {
         return;
       }
       fillRestaurantHTML();
-      callback(null, restaurant)
+      callback(null, restaurant);
     });
   }
-}
+};
 
 /**
  * Create restaurant HTML and add it to the webpage
@@ -85,22 +85,29 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const address = document.getElementById('restaurant-address');
   address.innerHTML = restaurant.address;
+  /* ARIA label for address */
+  let ariaLabelAdress = document.getElementById('address_label');
+  ariaLabelAdress.innerHTML = "Address: " + restaurant.address;
 
   const image = document.getElementById('restaurant-img');
-  image.className = 'restaurant-img'
-  image.alt = 'Photo of' + restaurant.name + ' restaurant in ' + restaurant.neighborhood + ',' + restaurant.cuisine_type +'cuisine';
+  image.className = 'restaurant-img';
+  /* Add Alt elements with title to images */
+  image.alt = 'Photo of' + restaurant.name + ' restaurant in ' + restaurant.neighborhood + ',' + restaurant.cuisine_type + 'cuisine';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
+  /* ARIA label for cuisine */
+  let ariaLabelCuisine = document.getElementById('cuisine_label');
+  ariaLabelCuisine.innerHTML = "Cuisine: " + restaurant.cuisine_type;
 
-  // fill operating hours
+  /* Fill operating hours */
   if (restaurant.operating_hours) {
     fillRestaurantHoursHTML();
   }
   // fill reviews
   fillReviewsHTML();
-}
+};
 
 /**
  * Create restaurant operating hours HTML table and add it to the webpage.
@@ -109,7 +116,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
   const hours = document.getElementById('restaurant-hours');
   for (let key in operatingHours) {
     const row = document.createElement('tr');
-    
+
     // Create Attitube for Tab Index on Row Only
     var label_tabindex = document.createAttribute("tabindex");
     label_tabindex.value = 0;
@@ -131,13 +138,13 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
 
     hours.appendChild(row);
 
-    // Aria Label for Row That Speaks Day + Hours
-    var aria_label = document.createElement('label');
-    aria_label.id = key + "_label";
-    aria_label.className = "aria-label";
-    aria_label.innerHTML = key + operatingHours[key];
+    /* Aria label for row that reads day and hours */
+    let ariaLabel = document.createElement('label');
+    ariaLabel.id = key + "_label";
+    ariaLabel.className = "aria-label";
+    ariaLabel.innerHTML = key + operatingHours[key];
   }
-}
+};
 
 /**
  * Create all reviews HTML and add them to the webpage.
@@ -159,13 +166,13 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
     ul.appendChild(createReviewHTML(review));
   });
   container.appendChild(ul);
-}
+};
 
 /**
  * Create review HTML and add it to the webpage.
  */
 createReviewHTML = (review) => {
-  
+
   // Set Review ID to the Next Random Number
   var randomNumberBetween0and19999 = Math.floor(Math.random() * 20000);
   var review_id = randomNumberBetween0and19999;
@@ -187,37 +194,37 @@ createReviewHTML = (review) => {
   comments.innerHTML = review.comments;
   li.appendChild(comments);
 
-    // Add Tab Index for the List Element
-    var label_tabindex = document.createAttribute("tabindex");
-    label_tabindex.value = 0;
-    // Set the attirubte to the row
-    li.setAttributeNode(label_tabindex);
+  // Add Tab Index for the List Element
+  var label_tabindex = document.createAttribute("tabindex");
+  label_tabindex.value = 0;
+  // Set the attirubte to the row
+  li.setAttributeNode(label_tabindex);
 
-    // Add Aria LabelledBy Attribute for Review
-    var label_attribute = document.createAttribute("aria-labelledby");
-    label_attribute.value = review_id + "_label";
-    li.setAttributeNode(label_attribute);
+  // Add Aria LabelledBy Attribute for Review
+  var label_attribute = document.createAttribute("aria-labelledby");
+  label_attribute.value = review_id + "_label";
+  li.setAttributeNode(label_attribute);
 
-    // Add Aria Label for Single Review
-    var aria_label = document.createElement('label');
-    aria_label.id = review_id + "_label";
-    aria_label.className = "aria-label";
-    aria_label.innerHTML = "Rating " + review.rating + " stars. Date " + review.date + ". Reviewed By " + review.name + ". Comments: " + review.comments;
+  // Add Aria Label for Single Review
+  var aria_label = document.createElement('label');
+  aria_label.id = review_id + "_label";
+  aria_label.className = "aria-label";
+  aria_label.innerHTML = "Rating " + review.rating + " stars. Date " + review.date + ". Reviewed By " + review.name + ". Comments: " + review.comments;
 
-    li.appendChild(aria_label);
+  li.appendChild(aria_label);
 
   return li;
-}
+};
 
 /**
  * Add restaurant name to the breadcrumb navigation menu
  */
-fillBreadcrumb = (restaurant=self.restaurant) => {
+fillBreadcrumb = (restaurant = self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
-}
+};
 
 /**
  * Get a parameter by name from page URL.
@@ -233,4 +240,4 @@ getParameterByName = (name, url) => {
   if (!results[2])
     return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
+};
