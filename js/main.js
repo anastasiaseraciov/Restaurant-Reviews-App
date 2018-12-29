c826aa0e67ecb46235afbe9c37b0c981a5f766af
Anastasia Seraciov <anastasia.seraciov@yahoo.com>
2018-12-29 02:50:31 +0000

    Delete Mapbox scripts

--- js/main.js
+++ js/main.js
@@ -71,7 +71,6 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 /**
  * Initialize leaflet map, called from HTML.
  */
-
 initMap = () => {
   self.newMap = L.map('map', {
         center: [40.722216, -73.987501],
